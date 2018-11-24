f1ab72ff44f31dce6ae0a548d80d8c7a6501776f
Ilnur Nasibullin <softilnyr16@gmail.com>
2018-11-24 12:32:05 +0300

    Default Changelist

--- src/hlt/Strategy.java
+++ src/hlt/Strategy.java
@@ -7,24 +7,43 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class Strategy {
+    /**
+     * дистанция между объектами
+     */
+    private GameMap gameMap;
 
-    public static double getDistance(Entity e1, Entity e2) {
+    public Strategy(GameMap gameMap) {
+        this.gameMap = gameMap;
+    }
+
+    private static double getDistance(Entity e1, Entity e2) {
         return Math.sqrt(Math.pow(e1.getXPos() - e2.getXPos(), 2) + Math.pow(e1.getYPos() - e2.getYPos(), 2));
     }
 
-    public Map<Planet, Double> getNearPlanet(Entity e, Map<Integer, Planet> planets) {
-        Map<Planet, Double> result = new TreeMap<>((o1, o2) -> (int) (Strategy.getDistance(e, o1) - Strategy.getDistance(e, o2)));
-        planets.forEach((integer, planet) -> {
-            result.put(planet, Strategy.getDistance(e, planet));
-
-        });
-        return result;
-    }
+//    public Map<Planet, Double> sortedPlanets(Entity e, Map<Integer, Planet> planets) {
+//        Map<Planet, Double> result = new TreeMap<>((o1, o2) -> (int) (Strategy.getDistance(e, o1) - Strategy.getDistance(e, o2)));
+//        planets.forEach((integer, planet) -> {
+//            result.put(planet, Strategy.getDistance(e, planet));
+//
+//        });
+//        return result;
+//    }
 
     //ближайшая планета
     //на вход список планет
     //
-
+    private static Planet getNearPlanet(Map<Integer, Planet> planets, Entity e1) {
+        Planet result = planets.get(0);
+        double min = Strategy.getDistance(e1, result);
+        for (Integer pId : planets.keySet()) {
+            double distance = Strategy.getDistance(e1, planets.get(pId));
+            if (distance < min) {
+                result = planets.get(pId);
+                min = distance;
+            }
+        }
+        return result;
+    }
 
 
     /**
@@ -33,9 +52,9 @@ public class Strategy {
      *         корабль
      */
 
-    /**
-     * дистанция между объектами
-     */
+
+
+
 
     //Захватчик
     //Перехватчик - сбивает в полёте
@@ -47,13 +66,12 @@ public class Strategy {
      * поиск ближайших планет
      */
     public DockMove shipStrategy(Map<Integer, Planet> planets, Ship ship){
-//       Planet planet =  getNearPlanet(ship,planets);
-       Planet planet =  new Planet();
-       return  new DockMove(ship, planet);
+       Planet planet =  getNearPlanet(planets, ship);
+       return new DockMove(ship, planet);
     }
 
-
-
-
-
+    public ThrustMove attackNearPlanet(Ship ship) {
+        Planet planet = getNearPlanet(gameMap.getAllPlanets(), ship);
+        return Navigation.navigateShipToDock(gameMap, ship, planet, Constants.MAX_SPEED);
+    }
 }
